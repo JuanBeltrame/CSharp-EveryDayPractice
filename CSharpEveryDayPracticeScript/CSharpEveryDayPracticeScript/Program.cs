e7274068aa84cee356ff1b7c275250e077ca38b3
Juan Beltrame <juan_beltrame@hotmail.com>
2024-03-09 15:07:34 -0300

    Update Program.cs

--- CSharpEveryDayPracticeScript/CSharpEveryDayPracticeScript/Program.cs
+++ CSharpEveryDayPracticeScript/CSharpEveryDayPracticeScript/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.Length; i++) {
+            for (int j = i + 1; j < nums.Length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    // Return the indices of the two numbers that add up to the target
+                    return new int[] { i, j };
+                }
+            }
+        }
+        // If no solution is found, return an empty array or handle it as needed
+        return new int[0];
+    }
+}
